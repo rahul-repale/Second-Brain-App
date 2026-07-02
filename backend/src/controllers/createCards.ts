768afd833a818fcc930fbb4497327aae2b8cddb8
Rahul Repale <workwithrahul14@gmail.com>
2026-07-02 13:32:55 +0000

    add create new card version1 logic

---
+++ backend/src/controllers/createCards.ts
@@ -0,0 +1,29 @@
+import type { Request, Response } from "express";
+import { CardModel, UserModel } from "../config/db.js";
+import mongoose from "mongoose";
+import type { CardInput } from "../types/types.js"
+
+export async function CreateCard(req: Request, res: Response){
+  if(!req.userId){
+    return res.status(500).json({ err: "Session Timeout, Login again" });
+  } 
+
+  const user = await UserModel.findOne({ _id: req.userId })
+  if(!user){
+    return res.status(403).json({ msg: "Invalid Credentials, Please Login again" })
+  }
+
+  const card: CardInput = {
+    createdBy: mongoose.Types.ObjectId.createFromHexString(req.userId),
+    title: req.body.title,
+    description: req.body.description,
+    tags: req.body.tags,
+    note: req.body.note,
+    editHistory: [{
+      userId: mongoose.Types.ObjectId.createFromHexString(req.userId),
+      userName: user.username as string
+    }]
+  }
+  const input = await CardModel.create(card)
+  res.status(200).json(input);
+}
