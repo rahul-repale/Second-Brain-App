3543d9c021b5b39df1c32d57f4b826a78f80b608
Rahul Repale <workwithrahul14@gmail.com>
2026-07-02 13:46:15 +0000

    add share brain link version1 logic

---
+++ backend/src/controllers/createShareLink.ts
@@ -0,0 +1,21 @@
+import type { Request, Response } from "express";
+import { LinkModel } from "../config/db.js";
+import type { LinkInput } from "../types/types.js"
+
+export async function createShareLink(req: Request, res: Response){
+  if(!req.userId){
+    return res.status(401).json({ err: "Session Timeout, Login again" });
+  } 
+  try{
+    const link: LinkInput = await LinkModel.create({
+      createdBy: req.userId,
+      targetedAt: req.body.cardId,
+      targetType: req.body.type,
+      permission: req.body.permission
+    })
+
+    return res.status(200).json({ link: link.token });
+  } catch(err) {
+    res.status(500).json({ msg: "Error, Please try again", err });
+  }
+}
