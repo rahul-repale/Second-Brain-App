15da62a6c04995b0ecb4fc8919c89ab945dda5ae
Rahul Repale <workwithrahul14@gmail.com>
2026-07-02 13:38:56 +0000

    add delete card version1 logic

---
+++ backend/src/controllers/deleteCard.ts
@@ -0,0 +1,15 @@
+import type { Request, Response } from "express";
+import { CardModel } from "../config/db.js";
+
+export async function deleteContent(req: Request, res: Response){
+  if(req.userId){
+    try{
+      await CardModel.deleteMany({ userId: req.userId, _id: req.body.cardId });
+      res.status(200).json({ msg: "Card deleted" });
+    } catch(err) {
+      res.status(500).json({ msg: "Error, Please try again", err });
+    }
+  } else {
+    return res.status(401).json({ err: "Session Timeout, Login again" });
+  }
+}
